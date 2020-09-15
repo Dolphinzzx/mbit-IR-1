3608427c7a80f67650e2e92ffb15678c7067525f
zhang <2448532184@qq.com>
2020-09-15 16:19:57 +0800

    2

---
+++ Mbit_IR.ts
@@ -0,0 +1,32 @@
+/*
+Copyright (C): 2020, Shenzhen Yahboom Tech
+modified by chengengyue
+*/
+
+/**
+ * IR remote
+ */
+//% icon="\uf1eb" weight=19 color=50
+namespace Mbit_IR {
+
+  /*
+    //% blockId=Mbit_IR_init
+    //% blockGap=20 weight=90
+    //% block="connect ir receiver to %pin"
+    //% shim=Mbit_IR::init
+    export function init(pin: Pins): void {
+      return
+    }
+
+
+    //% blockId=Mbit_IR_received_event
+    //% blockGap=20 weight=89
+    //% block="on |%btn| button pressed"
+    //% shim=Mbit_IR::onPressEvent
+    export function onPressEvent(btn: RemoteButton, body:Action): void {
+      return
+    }
+  */
+  
+  }
+  
